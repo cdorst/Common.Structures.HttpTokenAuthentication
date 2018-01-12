9833755c36883f8da3daa56f713b376bf6e039e1
Christopher Dorst <chris@dorst.email>
2018-01-12 11:44:02 -0600

    Add TokenAuthenticationCredentials

---
+++ Common.Structures.HttpTokenAuthentication/TokenAuthenticationCredentials.cs
@@ -0,0 +1,20 @@
+﻿using Common.Structures.HttpAuthentication;
+using System.Net.Http.Headers;
+
+namespace Common.Structures.HttpTokenAuthentication
+{
+    public class TokenAuthenticationCredentials : IHttpAuthentication
+    {
+        private const string token = nameof(token);
+
+        public TokenAuthenticationCredentials(string user, string secret)
+        {
+            Secret = secret;
+        }
+
+        public string Secret { get; set; }
+
+        public AuthenticationHeaderValue Header
+            => new AuthenticationHeaderValue(token, Secret);
+    }
+}
